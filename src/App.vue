61fcbf9f03d6c6921f6fc4dc96e1c21a331e5a77
0shuvo0 <seriouslolz1@gmail.com>
2022-02-17 08:10:49 +0600

    2 col(sidebar, main) layout setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="app">
-    hello
+    <Sidebar />
+    <div class="main">
+      hello
+    </div>
   </div>
 </template>
 
 <script>
+import Sidebar from "@/components/Sidevar.vue"
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Sidebar
+  }
 }
 </script>
+
+
+<style>
+#app{
+  display: flex;
+}
+.sidebar{
+  background: red;
+  width: 23.65%;
+
+}
+.main{
+  background: green;
+  flex-grow: 1;
+  margin-left: 1.8%;
+}
+</style>
